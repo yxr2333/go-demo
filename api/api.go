c42d6f2999917d1339382c911345dfc666a4d47a
sheep <54535282@qq.com>
2022-09-10 16:10:53 +0800

    tttt

--- api/api.go
+++ api/api.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"sheep/demo/security"
+	"sheep/demo/service"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes() *gin.Engine {
 	r := gin.Default()
+	r.GET("", security.JWTAuthMiddleWare(), service.TestService)
 	LoadUserApi(r)
 	LoadRoleApi(r)
 	LoadCommonApi(r)
--- service/service.go
+++ service/service.go
@@ -6,7 +6,9 @@ import (
 
 func TestService(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"code": 200,
-		"msg":  "Hello,World",
+		"code":     200,
+		"msg":      "Hello,World",
+		"uid":      c.GetString("uid"),
+		"username": c.GetString("username"),
 	})
 }
